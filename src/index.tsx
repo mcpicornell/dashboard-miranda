98a3098b978950f4460600db2010cab09524b434
mcpicornell <marc.castro.picornell@gmail.com>
2023-05-15 18:56:47 +0200

    typescript configuration tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App  from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import {store} from './app/store';
+import { store } from './app/store';
 import { UserContext } from './UserContext';
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
