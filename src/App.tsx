6e4f38d31f1980c7cc1d12b9c57ac28163886cce
mcpicornell <marc.castro.picornell@gmail.com>
2023-06-01 19:40:32 +0200

    hashRouter changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import BookingsPage from "./pages/bookings/BookingsPage";
@@ -41,18 +41,18 @@ export const initialState: State = {
 
 
 export const reducer = (state: typeof initialState, action: ReducerAction) => {
-if(action.type === "auth"){
-  state = {...state, auth: true, userName: action.value!.userName, email: action.value!.email};
-  return state;
-} 
-else if (action.type === "logOut"){
-  state = {...state, auth: false};
-  return state;
+  if (action.type === "auth") {
+    state = { ...state, auth: true, userName: action.value!.userName, email: action.value!.email };
+    return state;
+  }
+  else if (action.type === "logOut") {
+    state = { ...state, auth: false };
+    return state;
   }
 
-else{
-  state = {...state, auth: false};
-  return state;
+  else {
+    state = { ...state, auth: false };
+    return state;
   }
 }
 
@@ -60,36 +60,36 @@ else{
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <UserContext.Provider value ={{state, dispatch}}>
+    <UserContext.Provider value={{ state, dispatch }}>
 
-    <BrowserRouter>
+      <HashRouter >
 
         <Routes>
           <Route path="/login" element={<LoginPage />} />
 
-          <Route element={<PrivateRoute />}> 
+          <Route element={<PrivateRoute />}>
             <Route element={<Layout />}>
 
-                <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
 
-                <Route path="/bookings" element={<BookingsPage />} />
-                <Route path="/bookings/:id" element={<BookingsInfoPage/>} />
-                <Route path="/bookings/addBooking" element={<BookingAddPage/>} />
+              <Route path="/bookings" element={<BookingsPage />} />
+              <Route path="/bookings/:id" element={<BookingsInfoPage />} />
+              <Route path="/bookings/addBooking" element={<BookingAddPage />} />
 
-                <Route path="/rooms" element={<RoomsPage />}/>
-                <Route path='/rooms/addRoom' element={<RoomsAddPage />} />
+              <Route path="/rooms" element={<RoomsPage />} />
+              <Route path='/rooms/addRoom' element={<RoomsAddPage />} />
 
-                <Route path='/contact' element={<ContactPage />} />
+              <Route path='/contact' element={<ContactPage />} />
 
-                <Route path='/users' element={<UsersPage/>} /> 
-                <Route path='/users/addUser' element={<UsersAddPage/>} /> 
-                <Route path='/users/:id' element={<UsersEditPage/>} />
+              <Route path='/users' element={<UsersPage />} />
+              <Route path='/users/addUser' element={<UsersAddPage />} />
+              <Route path='/users/:id' element={<UsersEditPage />} />
             </Route>
           </Route>
         </Routes>
-      </BrowserRouter>
-      </UserContext.Provider>
-   
+      </HashRouter >
+    </UserContext.Provider>
+
   );
 }
 
